5a9c2f22994b59a3c1a410134395fe75b766634c
andi-nugroho <102736954+andi-nugroho@users.noreply.github.com>
2026-01-08 15:42:51 +0700

    added TodoWithUseEffect.jsx a very popular hook

---
+++ week6.1/experiments/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TodoWithUseEffect from './TodoWithUseEffect.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <TodoWithUseEffect/>
+)
